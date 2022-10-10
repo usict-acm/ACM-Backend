aab2a92b7b241e9f87251ba553c9e17b20d39b17
Kamakshi Nanda <kamakshinanda@Kamakshis-MacBook-Air.local>
2022-10-10 22:09:34 +0530

    Structure updated

---
+++ acm_dashboard_backend/index.js
@@ -0,0 +1,36 @@
+const express= require('express');
+// const eventRoutes = require('./routes/eventRoutes');
+const blogRoutes= require('./routes/blogRoutes');
+// const userRoutes = require('./routes/userRoutes');
+const mysql = require("mysql");
+
+const app=express();
+
+// app.use(eventRoutes);
+app.use(blogRoutes);
+// app.use(userRoutes);
+
+//function blogInit (){}
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.PASSWORD,
+  database: "acmbackend",
+});
+
+db.connect(function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("mysql connected");
+  }
+});
+
+app.get('/', (req,res) => {
+    // res.send(`Your email: ${req.user.email}`);
+    res.send("Hello");
+});
+
+app.listen(3000,() => {
+    console.log("listning on port 3000");
+});
---
+++ acm_dashboard_backend/routes/blogRoutes.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const router = express.Router();
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.PASSWORD,
+  database: "acmbackend",
+});
+
+db.connect(function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("mysql connected");
+  }
+});
+
+router.post("/blog/create", async(req, res) =>
+  db.query("SELECT * FROM blogs", function (err, results) {
+    if (!err) {
+      var sql =
+        "INSERT INTO blogs (userEmail, userName, blogTitle, content) VALUES ( 'gauranshi03@gmail.com', 'gauranshi', 'Blog1','this is first blog')";
+      db.query(sql, function (err, result) {
+        if (err) throw err;
+        //console.log("1 record inserted");
+        res.send(result);
+      });
+    } else {
+      //console.log(err);
+      console.log("Cannot post " + " /createBlog");
+    }
+  })
+);
+
+router.get("/blogs", async(req, res) => {
+  db.query("SELECT * FROM blogs", function (err, result, fields) {
+    if (err) throw err;
+    //console.log(result);
+    res.send(result);
+  });
+});
+
+router.post("/singleBlog/:blogId", function (req, res) {
+  const blogId = req.params.blogId;
+
+  if (isNaN(Number(blogId))) {
+    return res.status(400).json({ err: "Number only, please!" });
+  }
+
+  db.query(
+    `SELECT * FROM blogs WHERE blogId = ?`,
+    [blogId],
+    function (err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+router.post("/updateBlog", async(req, res) => {
+  var sql =
+    "UPDATE blogs SET blogTitle = 'Updated Title' WHERE userEmail = 'gauranshi3@gmail.com'";
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    //console.log(result.affectedRows + " record(s) updated");
+    res.send(result);
+  });
+});
+
+module.exports= router;
