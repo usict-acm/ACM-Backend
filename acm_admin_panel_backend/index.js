4b776234278469257b239752891eb6b16b0483fc
Aditya <adityasinghboss1234@gmail.com>
2022-11-13 21:36:28 +0530

    Joinus api added

--- acm_admin_panel_backend/index.js
+++ acm_admin_panel_backend/index.js
@@ -1,28 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const bodyparser = require("body-parser");
-const mysql = require("mysql");
+const joinUsRoutes = require("./routes/joinUsRoutes");
+
 
 
 const app = express();
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
-
-//Make connection with database
-const db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-});
-  
-db.connect(function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("mysql connected");
-    }
-});
+app.use(joinUsRoutes);
 
 
 
