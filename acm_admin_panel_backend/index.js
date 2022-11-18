c7c0933d8707a3d4aeafe41b7804192deff336d9
Gauranshi Gupta <gauranshigupta2000@gmail.com>
2022-11-18 08:01:44 +0530

    teamsRoutes added

--- acm_admin_panel_backend/index.js
+++ acm_admin_panel_backend/index.js
@@ -3,20 +3,16 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const joinUsRoutes = require("./routes/joinUsRoutes");
 const contactUsRoutes = require("./routes/contactUsRoutes");
-
-
+const teamRoutes = require("./routes/teamsRoutes");
 
 const app = express();
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 app.use(joinUsRoutes);
 app.use(contactUsRoutes);
-
-
+app.use(teamRoutes);
 
 //
 app.listen(3000, () => {
-    console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
-  
-  
