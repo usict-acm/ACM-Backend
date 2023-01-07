6c654b6cddf42666b0e2003d0bd89fc5477dcdd4
lunchspider <lunchspider03@gmail.com>
2023-01-08 00:09:47 +0530

    added database.js

---
+++ acm_admin_panel_backend/database.js
@@ -0,0 +1,10 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.ACMDATABASE,
+});
+
+module.exports = db
