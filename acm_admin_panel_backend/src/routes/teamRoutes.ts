16edcc822a72a8ab5f310c9a27df224d3a183051
lunchspider <lunchspider03@gmail.com>
2023-03-19 14:49:48 +0530

    added team route

--- acm_admin_panel_backend/src/routes/teamRoutes.ts
+++ acm_admin_panel_backend/src/routes/teamRoutes.ts
@@ -13,6 +13,18 @@ router.get("/team", async (_req, res, next) => {
         return next(new Exception(400, e.toString()));
     }
 });
+
+router.get("/team/:id", async (req, res, next) => {
+    try {
+        const id = Number(req.params.id);
+        let result = await prisma.team.findFirst({
+            where: { id: id }
+        });
+        res.json(result);
+    } catch (e: any) {
+        return next(new Exception(400, e.toString()));
+    }
+});
 router.post("/team", async (req, res, next) => {
     try {
         const data = req.body;
